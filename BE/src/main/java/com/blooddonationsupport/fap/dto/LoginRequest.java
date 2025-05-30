26d8ce086c680aef5c8b83c1b3b9347f25c2d9db
minhdienchau123 <minhloz1234567890@gmail.com>
2025-05-30 21:28:33 +0700

    update register and login

--- BE/src/main/java/com/blooddonationsupport/fap/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package BE.src.main.java.com.blooddonationsupport.fap.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String tenDangNhap;
-    private String matKhau;
-}
