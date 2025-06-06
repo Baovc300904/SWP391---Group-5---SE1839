62a9dad113b62bf4515c96d502f83b4307037bc0
2imAnhKhoa <99414353+AnhKhoaa157@users.noreply.github.com>
2025-06-07 00:08:44 +0700

    update

--- Front-End/src/components/Home/Hero/HeroSection.jsx
+++ Front-End/src/components/Home/Hero/HeroSection.jsx
@@ -11,13 +11,13 @@ export default function HeroSection() {
           className="hero-section"
         >
           <h1 className="hero-title">
-            Chào mừng đến với Hệ thống Hỗ trợ Hiến Máu
+            Chào mừng đến với Hiến Máu Cộng Đồng Việt
           </h1>
           <p className="hero-subtitle">
             Cùng nhau lan tỏa yêu thương, sẻ chia sự sống.
           </p>
           <p className="hero-description">
-            Website Hỗ trợ Hiến Máu là nền tảng kết nối giữa những tấm lòng nhân ái với các trung tâm y tế trên toàn quốc. 
+            Website Hiến Máu Cộng Đồng Việt là nền tảng kết nối giữa những tấm lòng nhân ái với các trung tâm y tế trên toàn quốc. 
             Chúng tôi cung cấp thông tin minh bạch, cập nhật về các sự kiện hiến máu, giúp bạn dễ dàng đăng ký, tìm kiếm lịch hiến máu phù hợp, 
             cũng như chia sẻ câu chuyện đầy cảm xúc từ cộng đồng. Cùng nhau, chúng ta lan tỏa sự sống và nhân văn đến từng nhịp tim.
           </p>
