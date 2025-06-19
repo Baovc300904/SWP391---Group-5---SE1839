86cd72b32d3616d668e4fcfcd5cf551b402d9e0b
2imAnhKhoa <99414353+AnhKhoaa157@users.noreply.github.com>
2025-06-19 10:00:00 +0700

    update

--- Back-End/src/main/java/com/demobe/demobe/enums/RequestStatus.java
+++
@@ -1,9 +0,0 @@
-package com.demobe.demobe.enums;
-
-public enum RequestStatus {
-    DANG_CHO,
-    DA_XAC_NHAN,
-    DA_HIEN,
-    HUY,
-    TU_CHOI
-}
--- Back-End/src/main/java/com/demobe/demobe/repository/BloodInventoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.demobe.demobe.repository;
-
-import com.demobe.demobe.entity.BloodInventory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BloodInventoryRepository extends JpaRepository<BloodInventory, Integer> {
-    // Các truy vấn tùy chỉnh có thể thêm sau
-}
