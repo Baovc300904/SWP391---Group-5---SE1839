c3d61547e4209002717299b8cbf636258bf9a4d6
HieuNewBie <hieu.phamtan2609@gmail.com>
2025-06-19 10:19:37 +0700

    update user
    thêm role

---
+++ Back-End (Final)/hienmauapi-main/src/main/java/org/fpt/blooddonate/repositories/UserRepository.java
@@ -0,0 +1,35 @@
+package org.fpt.blooddonate.repositories;
+
+import org.fpt.blooddonate.models.Blog;
+import org.fpt.blooddonate.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByTenDangNhap(String tenDangNhap);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByTenDangNhap(String tenDangNhap);
+
+    boolean existsByEmail(String email);
+
+    @Query("""
+    SELECT b FROM User b
+    WHERE (:role IS NULL OR b.vaiTro = :role)
+      AND (:keyword IS NULL OR LOWER(b.ten) LIKE LOWER(CONCAT('%', :keyword, '%')))          
+    """)
+    Page<User> paginated(
+            @Param("role") String role,
+            @Param("keyword") String keyword,
+            Pageable pageable
+    );
+}
