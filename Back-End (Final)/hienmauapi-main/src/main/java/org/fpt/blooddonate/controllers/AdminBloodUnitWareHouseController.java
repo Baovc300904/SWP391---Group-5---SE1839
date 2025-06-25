5fb145587706b5a3de3d3e9562c7b3abae96f1a1
minhdienchau123 <156238404+minhdienchau123@users.noreply.github.com>
2025-06-25 21:52:57 +0700

    update blood unit ware house

---
+++ Back-End (Final)/hienmauapi-main/src/main/java/org/fpt/blooddonate/controllers/AdminBloodUnitWareHouseController.java
@@ -0,0 +1,44 @@
+package org.fpt.blooddonate.controllers;
+
+import jakarta.validation.Valid;
+import org.fpt.blooddonate.dtos.requests.CancelBloodUnitWareHouseRequestDTO;
+import org.fpt.blooddonate.dtos.requests.ChangeStatusDonationRequestDTO;
+import org.fpt.blooddonate.dtos.requests.TestedBloodUnitWareHouseRequestDTO;
+import org.fpt.blooddonate.models.BloodUnitWareHouse;
+import org.fpt.blooddonate.services.BloodUnitWareHouseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/admin/blood-unit-warehouses")
+public class AdminBloodUnitWareHouseController {
+    @Autowired
+    private BloodUnitWareHouseService bloodUnitWareHouseService;
+
+    @GetMapping
+    public ResponseEntity<Page<BloodUnitWareHouse>> getAll(
+        @RequestParam(defaultValue = "1") int page,
+        @RequestParam(required = false) String status,
+        @RequestParam(required = false) String keyword
+    ) {
+        return ResponseEntity.ok(bloodUnitWareHouseService.getAll(page, status, keyword));
+    }
+
+    @PostMapping("/{id}/cancel")
+    public ResponseEntity<?> cancel(@PathVariable Integer id, @Valid @RequestBody CancelBloodUnitWareHouseRequestDTO payload) throws IOException {
+        return bloodUnitWareHouseService.cancel(id, payload)
+                .<ResponseEntity<?>>map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(404).body("Not found blood unit warehouse"));
+    }
+
+    @PostMapping("/{id}/tested")
+    public ResponseEntity<?> tested(@PathVariable Integer id, @Valid @RequestBody TestedBloodUnitWareHouseRequestDTO payload) throws IOException {
+        return bloodUnitWareHouseService.tested(id, payload)
+                .<ResponseEntity<?>>map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(404).body("Not found blood unit warehouse"));
+    }
+}
---
+++ Back-End (Final)/hienmauapi-main/src/main/java/org/fpt/blooddonate/services/BloodUnitWareHouseService.java
@@ -0,0 +1,53 @@
+package org.fpt.blooddonate.services;
+
+import org.fpt.blooddonate.configs.AppConfig;
+import org.fpt.blooddonate.dtos.requests.CancelBloodUnitWareHouseRequestDTO;
+import org.fpt.blooddonate.dtos.requests.TestedBloodUnitWareHouseRequestDTO;
+import org.fpt.blooddonate.models.BloodUnitWareHouse;
+import org.fpt.blooddonate.repositories.BloodUnitWareHouseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class BloodUnitWareHouseService {
+    @Autowired
+    private BloodUnitWareHouseRepository bloodUnitWareHouseRepository;
+
+    public Page<BloodUnitWareHouse> getAll(int page, String status, String keyword) {
+        Pageable pageable = PageRequest.of(page - 1, 20);
+        return bloodUnitWareHouseRepository.paginated(status, keyword, pageable);
+    }
+
+    public Optional<BloodUnitWareHouse> cancel(int id, CancelBloodUnitWareHouseRequestDTO payload) {
+        return bloodUnitWareHouseRepository.findById(id).map(bloodUnitWareHouse -> {
+            if (!bloodUnitWareHouse.getTrangThai().equals(AppConfig.BLOOD_UNIT_WAREHOUSE_WAIT_FOR_TESTING)) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Only cancel blood donate when it is wait for testing status");
+            }
+
+            bloodUnitWareHouse.setGhiChu(payload.getGhiChu());
+            bloodUnitWareHouse.setTrangThai(AppConfig.BLOOD_UNIT_WAREHOUSE_CANCEL);
+            return bloodUnitWareHouseRepository.save(bloodUnitWareHouse);
+        });
+    }
+
+    public Optional<BloodUnitWareHouse> tested(int id, TestedBloodUnitWareHouseRequestDTO payload) {
+        return bloodUnitWareHouseRepository.findById(id).map(bloodUnitWareHouse -> {
+            if (!bloodUnitWareHouse.getTrangThai().equals(AppConfig.BLOOD_UNIT_WAREHOUSE_WAIT_FOR_TESTING)) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Only tested blood donate when it is wait for testing status");
+            }
+
+            bloodUnitWareHouse.setKetQuaXetNghiem(payload.getKetQuaXetNghiem());
+            bloodUnitWareHouse.setTrangThai(AppConfig.BLOOD_UNIT_WAREHOUSE_READY);
+            bloodUnitWareHouse.setNgayHetHan(LocalDateTime.parse(payload.getNgayHetHan()));
+            return bloodUnitWareHouseRepository.save(bloodUnitWareHouse);
+        });
+    }
+}
